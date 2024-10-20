48af4b208e21f8f0e8866972435dc7154f4dd44c
Jorge <xxopxe@gmail.com>
2024-10-20 19:34:09 -0300

    remove XTAL_FIX We depend on v2.0.17 for micro-ros anyway.

--- micro_rosso.ino
+++ micro_rosso.ino
@@ -1,12 +1,4 @@
 
-// Needed on ESP32 Thing boards, Arduino ESP32 libs>=v3. Safe with v2.
-// https://github.com/espressif/arduino-esp32/issues/9837
-// If does not work, use Arduino ESP32 libs v2.017
-//#define ESP32_THING_XTAL_FIX
-#ifdef ESP32_THING_XTAL_FIX
-#include "soc/rtc.h"
-#endif
-
 #include "micro_rosso.h"
 
 #include "src/ticker.h"
@@ -21,12 +13,6 @@ RosStatus ros_status;
 void setup() {
 
   delay(2000);
-
-  #ifdef ESP32_THING_XTAL_FIX
-  rtc_clk_xtal_freq_update((rtc_xtal_freq_t)26);
-  rtc_clk_cpu_freq_set_xtal();
-  setCpuFrequencyMhz(240);
-  #endif
 
   D_SerialBegin(DEBUG_CONSOLE_BAUD, SERIAL_8N1, DEBUG_CONSOLE_PIN_RX, DEBUG_CONSOLE_PIN_TX);
 
